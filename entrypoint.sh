8ca6b38a44aa45e32db4971f5cd1de2b34becda8
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2020-03-08 23:44:12 +0000

    Issue-32
    Setup overcommit only for git repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,12 @@
 set -e
 
 composer install
-overcommit --install
-overcommit --sign
+
+# Setup overcommit only for git repo
+if git rev-parse --git-dir > /dev/null 2>&1; then
+    overcommit --install
+    overcommit --sign
+fi
+
 
 exec "$@"
